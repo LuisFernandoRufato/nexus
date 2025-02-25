a4add57f7452106b4404fcff3e1e5579abdcf9d7
LuisRufato <luis.rufato@gmail.com>
2025-02-25 15:06:28 -0300

    Código revisado

--- cypress/e2e/consulta.cy.js
+++ cypress/e2e/consulta.cy.js
@@ -7,13 +7,10 @@ describe('Nexus', () => {
         cy.get('div.relative > .absolute').click()
         cy.get('#consultation').click()
         cy.get('.grid > :nth-child(1) > .w-full').type('140068')
-        cy.get('.bg-gray-50.rounded-md > .grid > :nth-child(4) > div > .w-full').select('Boituva')
-        cy.get('.flex > .bg-purple-950').click()
-        cy.wait(3000)
-        cy.scrollTo('50%', '50%')
+        cy.get('.space-x-1 > .flex').click()
         cy.wait(2000)
-        cy.scrollTo('1%', '99%')
-        cy.wait(2000)
-        cy.clock()
+        cy.get('.bg-gray-600').click()
+        cy.wait(1000)
+        cy.scrollTo('99%', '1%')
     });
 });
